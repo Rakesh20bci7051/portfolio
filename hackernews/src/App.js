3f8a4f006eb7c7789d02c24425e44600488256bb
Rakesh Mehta <120373597+Rakesh20bci7051@users.noreply.github.com>
2023-12-11 22:55:38 +0530

    Update App.js

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
