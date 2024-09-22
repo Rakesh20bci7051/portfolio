925d90761e6ed2324b203fabef5842c5188d67ad
Rakesh Mehta <120373597+Rakesh20bci7051@users.noreply.github.com>
2024-09-22 11:42:19 +0530

    Update App.js

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Chatbot from './components/Chatbot';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Chatbot /> {/* Embed the Chatbot here */}
       <Contact />
     </div>
   );
