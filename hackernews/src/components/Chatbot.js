7b37cd2bcc35be82dfdb9af98db1b45bc34ebeb3
Rakesh Mehta <120373597+Rakesh20bci7051@users.noreply.github.com>
2024-09-22 11:40:51 +0530

    Create Chatbot.js

---
+++ hackernews/src/components/Chatbot.js
@@ -0,0 +1,18 @@
+// src/components/Chatbot.js
+import React from 'react';
+
+const Chatbot = () => {
+  return (
+    <div style={{ width: '100%', height: '700px' }}>
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/aMBuZhTJWKkc8iR2sdwyE"
+        width="100%"
+        style={{ height: '100%', minHeight: '700px' }}
+        frameBorder="0"
+        title="Chatbot"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Chatbot;
